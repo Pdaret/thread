fdd12da92a5b3e62c6fd504a88c78df0b3ff2615
Pdaret <mr.srezaei@outlook.com>
2022-11-17 16:04:07 +0330

    braining

--- psthread.c
+++ psthread.c
@@ -7,33 +7,27 @@
 //void *worker(void *args);
 
 
-int main(){
-    int child,grandchild;
-    child=fork();
-    if(child==0){
-        grandchild = fork();
-        if(grandchild == 0){
-            printf("GRANDCHILD pid=%d   PARETN pid=%d\n",getpid(),getppid());
-        }
-        else if(grandchild>0){
-        printf("CHILD pid=%d   PARETN pid=%d\n",getpid(),getppid());
-        }
-    }
-    else if(child>0){
-        int returnStatus;    
-        waitpid(grandchild, &returnStatus, 0);
+int main()
+{
+    pid_t parent,a,b;
 
-        if (returnStatus == 0)    
-        {
-            printf("The child process terminated normally.");    
-        }
-
-        if (returnStatus == 1)      
-        {
-            printf("The child process terminated with an error!.");    
-        }
-        printf("PARENT VALUE=%d\n",getpid());
+    parent = getpid();
+    fork();
+    a = getpid();
+    if( a!=parent )
+    {
+        fork();
+        b = getpid();
+        if( a==b )
+            printf("I am the : %d ppid: %d\n",a,getppid());
+        else
+            printf("I am the grand: %d ppid: %d\n",b,getppid());
     }
+    else
+    {
+        printf("I am the parent: %d\n",parent);
+    }
+    
     return(0);
 }
 
@@ -43,21 +37,4 @@ int main(){
     while (1) 
         printf("I am alive\n");
 }*/
-    /* pid_t parent,a,b;
 
-    parent = getpid();
-    fork();
-    a = getpid();
-    if( a==parent )
-    {
-        printf("I am the parent: %d\n",parent);
-    }
-    else
-    {
-        fork();
-        b = getpid();
-        if( a==b )
-            printf("I am the : %d ppid: %d\n",a,getppid());
-        else
-            printf("I am the grand: %d ppid: %d\n",b,getppid());
-    } */
