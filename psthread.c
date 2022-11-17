5923b518e04038badd5602e64db8833db18f6e4e
Pdaret <mr.srezaei@outlook.com>
2022-11-17 16:47:04 +0330

    final final finally forking :)

--- psthread.c
+++ psthread.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
-//#include <pthread.h>
+#include <pthread.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 
-//void *worker(void *args);
+void *worker(void *args);
 
 
 int main()
 {
+    pthread_t tid;
+    pthread_attr_t attr;
     pid_t parent,child,grandchild;
     parent = getpid();
     child = fork();
     if (child == 0)
     {
+        pthread_attr_init(&attr);
+        pthread_create(&tid,&attr,worker,NULL);
+        pthread_join(tid,NULL);
         grandchild = fork();
         if (grandchild == 0)
         {
@@ -28,24 +33,29 @@ int main()
         }
         
     }else{
-        
+        int cancelresult;
         int returnStatus;
         waitpid(grandchild,&returnStatus,0);
         if (WEXITSTATUS(returnStatus)== 0)
         {
             printf("grandchild exited normally\n");
         }else{
-            printf("grandchild exited with status: %d\n",returnStatus);
+            printf("grandchild exited with and error\n");
         }
-        printf("im in parent with pid: %d\n",getpid());
+        cancelresult = pthread_cancel(tid);
+        if(cancelresult==0){
+            printf("im in parent with pid: %d and thread canelled successfully...\n",getpid());
+        }else{
+            printf("im in parent with pid: %d but thread not canelled...\n",getpid());
+        }
     }
     return(0);
 }
 
 
 
-/*void *worker(void *args){
+void *worker(void *args){
     while (1) 
         printf("I am alive\n");
-}*/
+}
 
